fc0741da2b61aa59a52180f28adb2034de99cf91
KevinA-cpu <kevinanderson1410@gmail.com>
2022-12-20 15:14:06 +0700

    #get, getWithMaDH, insert, delete DonDatHang

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import Express from "express";
 import cors from "cors";
 
+import DonDatHangRouter from "./src/DonDatHang/routes.js";
+
 const App = Express();
 
 //Middlewares
@@ -11,5 +13,7 @@ App.use(cors());
 App.get("/", (req, res) => {
   res.send("placeholder");
 });
+
+App.use("/api/v1/DonDatHang", DonDatHangRouter);
 
 App.listen(3000, () => console.log(`App is listening on 3000`));
